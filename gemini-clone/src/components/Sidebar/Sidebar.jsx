76ffe756dd6ef042be1ad0a712471f8514bacbec
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-05-31 18:13:55 +0300

    i added bottom and icon for the sidebar

--- gemini-clone/src/components/Sidebar/Sidebar.jsx
+++ gemini-clone/src/components/Sidebar/Sidebar.jsx
@@ -7,9 +7,24 @@ const Sidebar = () => {
     return (
         <div className='Sidebar'>
             <div className="top">
-                <img src={assets.menu_icon} alt="" />
+                <img className='menu' src={assets.menu_icon} alt="" />
+                <div className="new-chat">
+                    <img src={assets.plus_icon} alt="" />
+                    <p>New Chat</p>
+                </div>
+                <div className="recent">
+                    <p className="recent-title">Recent</p>
+                    <div className="recent-entry">
+                        <img src={assets.message_icon} alt="" />
+                        <p>What is react ...</p>
+                    </div>
+                </div>
             </div>
-            <div className="bottom"></div>
+            <div className="bottom">
+                <div className="bottom-item">
+                    <img src={assets.question_icon} alt="" />
+                </div>
+            </div>
         </div>
     )
 }
