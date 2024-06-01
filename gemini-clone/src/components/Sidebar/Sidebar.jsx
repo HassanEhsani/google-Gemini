f3cf1778a9f65eff25b15c52e8060706142cc422
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-01 17:36:49 +0300

    i wrote the setextended for hide and show the icon

--- gemini-clone/src/components/Sidebar/Sidebar.jsx
+++ gemini-clone/src/components/Sidebar/Sidebar.jsx
@@ -10,7 +10,7 @@ const Sidebar = () => {
     return (
         <div className='sidebar'>
             <div className="top">
-                <img className='menu' src={assets.menu_icon} alt="" />
+                <img onClick={() => setExtended(prev =>!prev)} className='menu' src={assets.menu_icon} alt="" />
                 <div className="new-chat">
                     <img src={assets.plus_icon} alt="" />
                     {extended ? <p>New Chat</p> : null}
@@ -27,7 +27,7 @@ const Sidebar = () => {
             <div className="bottom">
                 <div className="bottom-item recent-entry">
                     <img src={assets.question_icon} alt="" />
-                    {extended ? <p>Help</p> :null}
+                    {extended ? <p>Help</p> : null}
                 </div>
                 <div className="bottom-item recent-entry">
                     <img src={assets.history_icon} alt="" />
@@ -35,7 +35,7 @@ const Sidebar = () => {
                 </div>
                 <div className="bottom-item recent-entry">
                     <img src={assets.setting_icon} alt="" />
-                   {extended ?  <p>Settings</p> : null}
+                    {extended ? <p>Settings</p> : null}
                 </div>
             </div>
         </div>
