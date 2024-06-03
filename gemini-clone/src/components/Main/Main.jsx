ec144bfdb8b54852db951c9c3fe119c8450ea982
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-06-03 23:37:57 +0300

    i wrote all of option about main in the main.jsx

--- gemini-clone/src/components/Main/Main.jsx
+++ gemini-clone/src/components/Main/Main.jsx
@@ -32,6 +32,7 @@ const Main = () => {
                         <img src={assets.code_icon} alt="" />
                     </div>
                 </div>
+                
                 <div className="main-bottom">
                     <div className="search-box">
                         <input type="text" placeholder="Enter a promp here" />
